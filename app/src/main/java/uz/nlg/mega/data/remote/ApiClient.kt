fc8c731b128ea43d78120f5af8c0fd28943b4c31
Bogibek <bogibekdev@gmail.com>
2023-11-01 19:49:52 +0500

    Commit Information:

--- app/src/main/java/uz/nlg/mega/data/remote/ApiClient.kt
+++ app/src/main/java/uz/nlg/mega/data/remote/ApiClient.kt
@@ -7,11 +7,7 @@ object ApiClient {
     private const val SERVER_DEVELOPMENT = "https://megaapi.nextlevelgroup.uz/api/v1/"
     private const val SERVER_PRODUCTION = "https://megaapi.nextlevelgroup.uz/api/v1/"
 
-    fun server(): String {
-        return if (IS_TESTER)
-            SERVER_DEVELOPMENT
-        else
-            SERVER_PRODUCTION
-    }
+    fun server() = if (IS_TESTER) SERVER_DEVELOPMENT else SERVER_PRODUCTION
+
 
 }
